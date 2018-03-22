095f94a65836fb9f19f2d4fc9809c10dfc266c08
DrPing <cedric.ogire@hotmail.fr>
2018-03-22 16:49:01 +0100

    INIT_PLAYER: replace useless function for random nbr

--- src/init_player.c
+++ src/init_player.c
@@ -7,22 +7,16 @@
 
 #include "lemipc.h"
 
-int get_rand_nbr()
-{
-	int nbr = rand() % MAP_SIZE + 0;
-	return nbr;
-}
-
 map_t *init_player(map_t *map_struct, char *team_char)
 {
 	srand(time(NULL));
-	int x = get_rand_nbr();
-	int y = get_rand_nbr();
+	int x = rand() % MAP_SIZE + 0;
+	int y = rand() % MAP_SIZE + 0;
 	printf("x: %d\ny: %d\n", x, y);
 	int team_nbr = atoi(team_char);
 	pid_t pid = getpid();
 	map_struct->map[x][y].team_nbr = team_nbr;
 	map_struct->map[x][y].pid = pid;
-	printf("map[x][y].team_nbr : %ld", map_struct->map[x][y].team_nbr);
+	printf("map[x][y].team_nbr : %ld\n", map_struct->map[x][y].team_nbr);
 	return map_struct;
 }
