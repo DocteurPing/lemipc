69d85eccd46928ab30c3e38d04bcb85f1cb7595e
drping <cedric.ogire@epitech.eu>
2018-03-23 10:23:16 +0100

    SEMAPHORE: optimizing code

--- src/my_semaphore.c
+++ src/my_semaphore.c
@@ -7,28 +7,24 @@
 
 #include "lemipc.h"
 
-bool	get_access_memory(int id)
+void	get_access_memory(int id)
 {
 	struct sembuf	sops;
 
-	if (semctl(id, 0, GETVAL) == 0)
-		return (false);
 	sops.sem_num = 0;
 	sops.sem_flg = 0;
 	sops.sem_op = -1;
 	semop(id, &sops, 1);
-	return (true);
 }
 
-bool	left_memory_access(int id)
+void	left_memory_access(int id)
 {
 	struct sembuf	sops;
 
 	if (semctl(id, 0, GETVAL) == 1)
-		return (false);
+		return ;
 	sops.sem_num = 0;
 	sops.sem_flg = 0;
 	sops.sem_op = 1;
 	semop(id, &sops, 1);
-	return (true);
 }
