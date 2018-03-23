f1196ed9efd1f51c4fcb9fdf00858ae2b2576c51
drping <cedric.ogire@epitech.eu>
2018-03-23 10:22:47 +0100

    INIT_PLAYER: forbid superposition of two player when initiate

--- src/init_player.c
+++ src/init_player.c
@@ -10,23 +10,19 @@
 map_t *init_player(lemipc_t *lemipc, char *team_char)
 {
 	map_t *tmp = (map_t *)lemipc->addr;
-	srand(time(NULL));
 	int x = rand() % MAP_SIZE + 0;
 	int y = rand() % MAP_SIZE + 0;
-	printf("x: %d\ny: %d\n", x, y);
 	int team_nbr = atoi(team_char);
 	pid_t pid = getpid();
 
-	if (get_access_memory(lemipc->sem_id)) {
-		tmp->map[x][y].team_nbr = team_nbr;
-		tmp->map[x][y].pid = pid;
-		left_memory_access(lemipc->sem_id);
-		#ifdef DEBUG
-			printf("map[x][y].team_nbr : %ld\n",
-				((map_t *)lemipc)->map[x][y].team_nbr);
-		#endif
-	} else {
-		lemipc->addr = init_player(lemipc, team_char);
+	srand(time(NULL));
+	get_access_memory(lemipc->sem_id);
+	while (tmp->map[x][y].team_nbr != 0) {
+		int x = rand() % MAP_SIZE + 0;
+		int y = rand() % MAP_SIZE + 0;
 	}
+	tmp->map[x][y].team_nbr = team_nbr;
+	tmp->map[x][y].pid = pid;
+	left_memory_access(lemipc->sem_id);
 	return (map_t *)lemipc->addr;
 }
