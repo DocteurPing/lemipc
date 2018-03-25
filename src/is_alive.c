53ba1cf71a37c4f492485092757ea8fc5a2c5282
Sarah <sarah.godard@epitech.eu>
2018-03-25 15:37:28 +0200

    IS_ALIVE: norme corrected

--- src/is_alive.c
+++ src/is_alive.c
@@ -17,8 +17,8 @@ int check_others(lemipc_t *lemipc, int team_nbr)
 	for (int i = y > 0 ? -1 : 0; i <= 1; i++) {
 		for (int j = x > 0 ? -1 : 0; j <= 1; j++) {
 			if (j + x < MAP_SIZE && i + y < MAP_SIZE) {
-				int current_team = tmp->map[y + i][x + j].team_nbr;
-				if (current_team == team_nbr) {
+				int cteam = tmp->map[y + i][x + j].team_nbr;
+				if (cteam == team_nbr) {
 					count += 1;
 				}
 			}
@@ -30,19 +30,17 @@ int check_others(lemipc_t *lemipc, int team_nbr)
 bool is_alive(lemipc_t *lemipc)
 {
 	map_t *tmp = (map_t *)lemipc->addr;
-	int count = 0;
 	int y = lemipc->pos.y;
 	int x = lemipc->pos.x;
-	int team_nbr = tmp->map[y][x].team_nbr;
+	int tn = tmp->map[y][x].team_nbr;
 
 	for (int i = y > 0 ? -1 : 0; i <= 1; i++) {
 		for (int j = x > 0 ? -1 : 0; j <= 1; j++) {
 			if (j + x < MAP_SIZE && i + y < MAP_SIZE) {
-				int enemy_team = tmp->map[y + i][x + j].team_nbr;
-				if (enemy_team != 0 && enemy_team != team_nbr) {
-					count = check_others(lemipc, enemy_team);
+				int eteam = tmp->map[y + i][x + j].team_nbr;
+				if (eteam != 0 && eteam != tn) {
+					int count = check_others(lemipc, eteam);
 					if (count > 1) {
-						printf("enemy_team : %d\n", enemy_team);
 						return false;
 					}
 					count = 0;
