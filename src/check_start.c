3cbba81a008cd0a2d6d3bae1d796ef75b05eae8d
drping <cedric.ogire@epitech.eu>
2018-03-25 17:21:45 +0200

    START: fix condition of strating

--- src/check_start.c
+++ src/check_start.c
@@ -13,8 +13,14 @@ bool check_start(map_t *map)
 	int i = 0;
 	int check = 0;
 
-	for (; tmp[i].team != -1; i++)
+	for (; tmp[i].team != -1; i++) {
+		#ifdef DEBUG
+			printf("team found: %d | nbr: %d\n",
+				tmp[i].team, tmp[i].nbr);
+		#endif
+		if (tmp[i].nbr > 1)
 			check++;
+	}
 	if (check > 1)
 		return (true);
 	return (false);
